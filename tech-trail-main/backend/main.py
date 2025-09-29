722e70c6cc72cfae570cd33095eb217eeca34ea6
TECH IN MY STYLE <techinmystyle@gmail.com>
2025-09-29 21:45:41 +0530

    Update main.py

--- tech-trail-main/backend/main.py
+++ tech-trail-main/backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # Enable CORS (adapt allow_origins for production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://techinmystyle.com", "http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["*"],  # This allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
